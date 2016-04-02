4e9193029265abf338b2e442d5ae2eff253fbea8
Sagar Singh <ssing128@gmail.com>
2016-04-01 17:10:32 -0700

    fetch data from server

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Product from './Product.jsx';
+import $ from 'jquery';
+
+const productsUrl = 'http://localhost:3000/products';
 
 export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: props.products
+    };
+  }
+
+  componentDidMount() {
+    $.getJSON(productsUrl, (data) => {
+      this.setState({
+        products: data.products
+      });
+    });
+  }
+
 	render() {
 		return (
 			<div>
         <h2>Products</h2>
         <ul className="collection">
-          {this.props.products.map(function(product,i) {
+          {this.state.products.map(function(product,i) {
             return <Product key={product.name} product={product} />
           },this)}
         </ul>
