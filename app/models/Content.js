441fbef78727432070959166340b04df7ec57d97
Sagar Singh <ssing128@gmail.com>
2016-04-12 02:47:51 -0700

    model update

--- app/models/Content.js
+++ app/models/Content.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ContentSchema = new Schema({
-  link: {type: String},
-  products: [{type: Schema.ObjectId, ref: 'Product'}]
+  source: {type: String},
+  product_id: {type: Schema.ObjectId}
 });
 
 const Content = mongoose.model('Content', ContentSchema);
