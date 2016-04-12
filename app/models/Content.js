820de35496a1eceeccf868081ac701f81adcbc50
Sagar Singh <ssing128@gmail.com>
2016-04-11 19:59:28 -0700

    refrencing Product in Content model

--- app/models/Content.js
+++ app/models/Content.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const ContentSchema = new Schema({
   link: {type: String},
-  products: [{type: Schema.ObjectId}]
+  products: [{type: Schema.ObjectId, ref: 'Product'}]
 });
 
 const Content = mongoose.model('Content', ContentSchema);
