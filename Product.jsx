fd1d5a91b77ebbec2b8107a66a7e9e96329b68ab
Sagar Singh <ssing128@gmail.com>
2016-04-14 19:07:02 -0700

    front end routing: handled by react-router

--- Product.jsx
+++ Product.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {render} from 'react-dom';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 
 export default class Product extends React.Component {
 	render() {
 		return (
       <li className="collection-item">
-        {this.props.product.name + " " + this.props.product.release_date}
+        <Link
+          to="/products/:id">
+          {this.props.product.name + " " + this.props.product.release_date}
+        </Link>
       </li>
 		);
 	}
--- index.jsx
+++ index.jsx
@@ -1,9 +1,12 @@
 require("./node_modules/materialize-css/dist/css/materialize.min.css")
 require("./node_modules/materialize-css/dist/js/materialize.js");
 import React from 'react';
+import { render } from 'react-dom'
 import ReactDOM from 'react-dom';
 import ProductList from './ProductList';
 import ContentForm from './ContentForm';
+import Product from './Product';
+import { Router, Route, Link, hashHistory } from 'react-router'
 
 const productsUrl = 'http://localhost:3000/products';
 
@@ -41,6 +44,11 @@ export class App extends React.Component {
   }
 
 }
+render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="products/:id" component={ProductPage}/> 
+	</Router>
+), document.querySelector("#myApp"))
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
 
